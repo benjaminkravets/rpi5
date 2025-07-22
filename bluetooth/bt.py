ff819aeb0e1401383972b1c1b8f1c2c8fa08c2a0
Benjamin Kravets <bakravets@gmail.com>
2025-07-21 20:58:04 -0400

    retreive kernel version from dbus

--- bluetooth/bt.py
+++ bluetooth/bt.py
@@ -1,1 +1,13 @@
-print("Hello")
+#!/usr/bin/python3
+import dbus
+
+bus = dbus.SystemBus()  #connect to DBus
+
+proxy = bus.get_object('org.freedesktop.hostname1', '/org/freedesktop/hostname1')
+
+interface = dbus.Interface(proxy, 'org.freedesktop.DBus.Properties')
+
+hostname = interface.Get('org.freedesktop.hostname1', 'KernelVersion')
+#hostname = interface.GetAll('org.freedesktop.hostname1')
+
+print(hostname)
